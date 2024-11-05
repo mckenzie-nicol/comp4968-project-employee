09dc968b36d92e6cf4cd53d252cb5a31eaa7ee8c
iwen-sue <grace2268@hotmail.com>
2024-11-04 19:03:57 -0800

    add shadcn button component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Button } from "@/components/ui/button"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <Button>Click me</Button>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

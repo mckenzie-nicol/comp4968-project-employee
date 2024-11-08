386a1041a4ebd21de40a17265fb641ce3afbfd6d
jake currie <jakecurrie1@gmail.com>
2024-11-07 16:33:35 -0800

    add env config and load func

--- viz-service/config/config.go
+++ viz-service/config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	ReactAppUrl string
+	DbEndpoint  string
+	DbName      string
+	DbARN       string
+	DbPort      string
+	DbUser      string
+	DbPassword  string
+}
+
+func Load() *Config {
+	return &Config{
+		ReactAppUrl: os.Getenv("REACT_APP_URL"),
+		DbEndpoint:  os.Getenv("DB_ENDPOINT"),
+		DbName:      os.Getenv("DB_NAME"),
+		DbARN:       os.Getenv("DB_ARN"),
+		DbPort:      os.Getenv("DB_PORT"),
+		DbUser:      os.Getenv("DB_USER"),
+		DbPassword:  os.Getenv("DbPassword"),
+	}
+}

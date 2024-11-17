691d4415b268b34e1de61c091fc6ed4734358fbb
mckenzie-nicol <122124971+mckenzie-nicol@users.noreply.github.com>
2024-11-16 21:31:31 -0800

    Create add project button Configure modal to get name, hours, est start, est end Add date picker element for dates

---
+++ app/src/components/ui/date-picker.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import Calendar from "./calendar";
+
+interface DatePickerProps {
+  selected: Date | undefined;
+  setSelected: (date: Date | undefined) => void;
+}
+
+export function DatePicker({selected, setSelected}: DatePickerProps) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !selected && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {selected ? format(selected, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar selected={selected} setSelected={setSelected} />
+      </PopoverContent>
+    </Popover>
+  );
+}

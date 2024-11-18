7c616a91c39d53a290ce676c319c6e463546857d
iwen-sue <grace2268@hotmail.com>
2024-11-18 12:39:54 -0800

    add userId from session

--- app/src/pages/timesheet.tsx
+++ app/src/pages/timesheet.tsx
@@ -1,13 +1,16 @@
 //timesheet page
 import { TimesheetTable } from "@/components/timesheet/timesheet-form";
 
+// get userId from session storage
+const userId = sessionStorage.getItem("userId") ?? "5131efb8-4579-492d-97fd-49602e6ed513";
+
 export default function Timesheet() {
   return (
     <div className="p-6">
         <div className="flex items-center gap-4 mb-6">
           <h1 className="text-3xl font-bold">Timesheet</h1>
         </div>
-        <TimesheetTable employee_id="5131efb8-4579-492d-97fd-49602e6ed513"/>
+        <TimesheetTable employee_id={userId}/>
       </div>
   );
 }

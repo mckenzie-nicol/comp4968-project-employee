c02fc41c79ffe7e2ac8a24b4caef09eb30e42adc
Collin <omiyage4949@gmail.com>
2024-11-23 11:08:07 -0800

    add reject dialog

--- app/src/components/project/approval-table.tsx
+++ app/src/components/project/approval-table.tsx
@@ -9,6 +9,13 @@ import {
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import {
   Timesheet,
   HoursRecord,
 } from "@/components/project/manager-approval-layout";
@@ -75,6 +82,8 @@ function ApprovalTable({
   refetchData: () => void;
 }) {
   const [isChangingStatus, setIsChangingStatus] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [entryToReject, setEntryToReject] = useState<string>("");
 
   const handleApproveClick = async (id: string, approved: boolean) => {
     setIsChangingStatus(true);
@@ -83,110 +92,140 @@ function ApprovalTable({
     setIsChangingStatus(false);
   };
 
-  const handleRejectClick = async (id: string) => {
+  const handleRejectClick = async () => {
     setIsChangingStatus(true);
-    await updateSubmissionStatus(id);
+    await updateSubmissionStatus(entryToReject!);
     refetchData();
     setIsChangingStatus(false);
+    setIsDialogOpen(false);
   };
 
   console.log(trackedHours);
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Name</TableHead>
-          <TableHead>Hours To Add</TableHead>
-          <TableHead>Tracked Hours</TableHead>
-          <TableHead>Regular Hours</TableHead>
-          <TableHead>Overtime Hours</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {timesheets.map((entry, index) => (
-          <TableRow key={entry.id}>
-            <TableCell>{`${entry.first_name} ${entry.last_name}`}</TableCell>
-            <TableCell>
-              {!entry.approved
-                ? Object.values(entry.hours)
-                    .reduce((acc, curr) => acc + (parseFloat(curr) || 0), 0)
-                    .toFixed(2)
-                : ""}
-            </TableCell>
-            <TableCell>
-              {trackedHours[index]
-                ?.map((timeRecord: HoursRecord) => {
-                  const start = new Date(
-                    `2024-01-01T${timeRecord.start_time}:00`
-                  );
-                  const end = new Date(`2024-01-01T${timeRecord.end_time}:00`);
-                  return differenceInMinutes(end, start) / 60;
-                })
-                .reduce((acc: number, curr: number) => acc + curr, 0)
-                .toFixed(2) ?? ""}
-            </TableCell>
-            <TableCell>
-              {trackedHours[index]
-                ?.map((timeRecord: HoursRecord) => {
-                  const start = new Date(
-                    `2024-01-01T${timeRecord.start_time}:00`
-                  );
-                  let end = new Date(`2024-01-01T${timeRecord.end_time}:00`);
-                  if (end >= new Date(`2024-01-01T17:00:00`)) {
-                    end = new Date(`2024-01-01T17:00:00`);
-                  }
-                  return differenceInMinutes(end, start) / 60;
-                })
-                .reduce((acc: number, curr: number) => acc + curr, 0)
-                .toFixed(2) ?? ""}
-            </TableCell>
-            <TableCell>
-              {trackedHours[index]
-                ?.filter((timeRecord: HoursRecord) => {
-                  const end = new Date(`2024-01-01T${timeRecord.end_time}:00`);
-                  return end >= new Date(`2024-01-01T17:00:00`);
-                })
-                .map((timeRecord: HoursRecord) => {
-                  const start = new Date(`2024-01-01T17:00:00`);
-                  const end = new Date(`2024-01-01T${timeRecord.end_time}:00`);
-                  return differenceInMinutes(end, start) / 60;
-                })
-                .reduce((acc: number, curr: number) => acc + curr, 0)
-                .toFixed(2) ?? ""}
-            </TableCell>
-            <TableCell>{entry.approved ? "Approved" : "Open"}</TableCell>
-            <TableCell>
-              <div className="flex items-center space-x-3">
-                <Button
-                  variant="ghost"
-                  onClick={() => handleApproveClick(entry.id, entry.approved)}
-                  disabled={isChangingStatus}
-                  size="icon"
-                >
-                  {entry.approved ? (
-                    <Undo className="h-4 w-4" />
-                  ) : (
-                    <Check className="h-4 w-4" />
-                  )}
-                </Button>
-                <Button
-                  variant="ghost"
-                  onClick={() => handleRejectClick(entry.id)}
-                  disabled={isChangingStatus || entry.approved}
-                  size="icon"
-                >
-                  <X className="h-4 w-4" />
-                </Button>
-              </div>
-            </TableCell>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Hours To Add</TableHead>
+            <TableHead>Tracked Hours</TableHead>
+            <TableHead>Regular Hours</TableHead>
+            <TableHead>Overtime Hours</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Actions</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <div className="h-screen"></div>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {timesheets.map((entry, index) => (
+            <TableRow key={entry.id}>
+              <TableCell>{`${entry.first_name} ${entry.last_name}`}</TableCell>
+              <TableCell>
+                {!entry.approved
+                  ? Object.values(entry.hours)
+                      .reduce((acc, curr) => acc + (parseFloat(curr) || 0), 0)
+                      .toFixed(2)
+                  : ""}
+              </TableCell>
+              <TableCell>
+                {trackedHours[index]
+                  ?.map((timeRecord: HoursRecord) => {
+                    const start = new Date(
+                      `2024-01-01T${timeRecord.start_time}:00`
+                    );
+                    const end = new Date(
+                      `2024-01-01T${timeRecord.end_time}:00`
+                    );
+                    return differenceInMinutes(end, start) / 60;
+                  })
+                  .reduce((acc: number, curr: number) => acc + curr, 0)
+                  .toFixed(2) ?? ""}
+              </TableCell>
+              <TableCell>
+                {trackedHours[index]
+                  ?.map((timeRecord: HoursRecord) => {
+                    const start = new Date(
+                      `2024-01-01T${timeRecord.start_time}:00`
+                    );
+                    let end = new Date(`2024-01-01T${timeRecord.end_time}:00`);
+                    if (end >= new Date(`2024-01-01T17:00:00`)) {
+                      end = new Date(`2024-01-01T17:00:00`);
+                    }
+                    return differenceInMinutes(end, start) / 60;
+                  })
+                  .reduce((acc: number, curr: number) => acc + curr, 0)
+                  .toFixed(2) ?? ""}
+              </TableCell>
+              <TableCell>
+                {trackedHours[index]
+                  ?.filter((timeRecord: HoursRecord) => {
+                    const end = new Date(
+                      `2024-01-01T${timeRecord.end_time}:00`
+                    );
+                    return end >= new Date(`2024-01-01T17:00:00`);
+                  })
+                  .map((timeRecord: HoursRecord) => {
+                    const start = new Date(`2024-01-01T17:00:00`);
+                    const end = new Date(
+                      `2024-01-01T${timeRecord.end_time}:00`
+                    );
+                    return differenceInMinutes(end, start) / 60;
+                  })
+                  .reduce((acc: number, curr: number) => acc + curr, 0)
+                  .toFixed(2) ?? ""}
+              </TableCell>
+              <TableCell>{entry.approved ? "Approved" : "Open"}</TableCell>
+              <TableCell>
+                <div className="flex items-center space-x-3">
+                  <Button
+                    variant="ghost"
+                    onClick={() => handleApproveClick(entry.id, entry.approved)}
+                    disabled={isChangingStatus}
+                    size="icon"
+                  >
+                    {entry.approved ? (
+                      <Undo className="h-4 w-4" />
+                    ) : (
+                      <Check className="h-4 w-4" />
+                    )}
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    onClick={() => {
+                      setIsDialogOpen(true);
+                      setEntryToReject(entry.id);
+                    }}
+                    disabled={isChangingStatus || entry.approved}
+                    size="icon"
+                  >
+                    <X className="h-4 w-4" />
+                  </Button>
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <div className="h-screen"></div>
+      </Table>
+
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Confirm Rejection</DialogTitle>
+          </DialogHeader>
+          <div>Are you sure you want to reject this timesheet?</div>
+          <DialogFooter>
+            <Button
+              type="submit"
+              disabled={isChangingStatus}
+              onClick={() => handleRejectClick()}
+            >
+              Confirm
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
 

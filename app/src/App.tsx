1ee48708b16832d8d34b82b07cef93beb712989b
jake currie <jakecurrie1@gmail.com>
2024-11-29 20:35:09 -0800

    wrap app with theme provider

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./providers/theme-provider";
 import Home from "./pages/home";
 import { Navbar } from './components/layout/navbar'
 import { Footer } from './components/layout/footer'
 import { ApproveTimesheets } from "@/components/dashboard/approve-timesheets";
-
 import Timesheet from "./pages/timesheet";
 import Test from "./pages/test";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* timesheet route */}
-        <Route path="/timesheet" element={<Timesheet/>}></Route>
-        <Route path="/approve-timesheets" element={<ApproveTimesheets />} />
-        <Route path="/test" element={<Test />} />
-      </Routes>
-       <Footer />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* timesheet route */}
+          <Route path="/timesheet" element={<Timesheet/>}></Route>
+          <Route path="/approve-timesheets" element={<ApproveTimesheets />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+         <Footer />
+      </Router>
+    </ThemeProvider>
   );
 }
 

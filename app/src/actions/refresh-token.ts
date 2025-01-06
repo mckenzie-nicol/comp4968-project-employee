65372269e26481049b73398b74dd1d1b12fe1c54
mckenzie-nicol <122124971+mckenzie-nicol@users.noreply.github.com>
2025-01-06 11:31:04 -0800

    update refreshtoken to just return null

--- app/src/actions/refresh-token.ts
+++ app/src/actions/refresh-token.ts
@@ -1,14 +1,3 @@
 export default async function refreshTokens() {
-const refreshToken = sessionStorage.getItem('refreshToken');
-  const response = await fetch('/auth/refresh-token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ refresh_token: refreshToken }),
-  });
-  if (response.ok) {
-    const data = await response.json();
-    sessionStorage.setItem('accessToken', data.tokens.accessToken);
-    sessionStorage.setItem('tokenExpiry', (Date.now() + data.expiresIn * 1000).toString());
-    return data.tokens.accessToken;
-  } 
+return null;
 }
